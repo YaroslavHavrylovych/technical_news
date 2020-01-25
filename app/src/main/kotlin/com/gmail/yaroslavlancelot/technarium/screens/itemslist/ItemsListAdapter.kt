5d66e532928850e2e080f81d1fbceaf6843477a1
Yaroslav Havrylovych <yaroslavlancelot@gmail.com>
2020-01-25 17:10:55 +0200

    Design: items list has a publication date

--- app/src/main/kotlin/com/gmail/yaroslavlancelot/technarium/screens/itemslist/ItemsListAdapter.kt
+++ app/src/main/kotlin/com/gmail/yaroslavlancelot/technarium/screens/itemslist/ItemsListAdapter.kt
@@ -23,18 +23,30 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.Adapter
+import com.gmail.yaroslavlancelot.technarium.R
 import com.gmail.yaroslavlancelot.technarium.R.layout
 import com.gmail.yaroslavlancelot.technarium.data.local.items.posts.PostEntity
 import com.gmail.yaroslavlancelot.technarium.utils.extensions.getImage
 import com.gmail.yaroslavlancelot.technarium.screens.itemslist.ItemsListAdapter.ItemViewHolder
 import kotlinx.android.synthetic.main.lt_items_list_item.view.*
+import timber.log.Timber
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
 
 class ItemsListAdapter<T : PostEntity>(
     private val items: List<T>?,
     private val onItemClickListener: (item: T) -> Unit
 ) : Adapter<ItemViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder =
-        ItemViewHolder(LayoutInflater.from(parent.context).inflate(layout.lt_items_list_item, parent, false))
+    private lateinit var dateFormat: SimpleDateFormat
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        dateFormat = SimpleDateFormat(
+            parent.resources.getString(R.string.item_date_format),
+            Locale(parent.resources.getString(R.string.item_date_language), parent.resources.getString(R.string.item_date_country))
+        )
+        return ItemViewHolder(LayoutInflater.from(parent.context).inflate(layout.lt_items_list_item, parent, false))
+    }
 
     override fun getItemCount(): Int = items?.size ?: 0
 
@@ -43,10 +55,23 @@ class ItemsListAdapter<T : PostEntity>(
         holder.title.text = items[position].title
         holder.image.setImageResource(items[position].getImage())
         holder.itemView.setOnClickListener { onItemClickListener(items[position]) }
+        val date = items[position].pubDate.forList()
+        if (date == null) holder.date.setText(R.string.item_date_unknown)
+        else holder.date.text = date
     }
 
     class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val title: TextView = itemView.title_view
         val image: ImageView = itemView.image_view
+        val date: TextView = itemView.pub_date_view
+    }
+
+    private fun Date.forList(): String? {
+        try {
+            return dateFormat.format(this)
+        } catch (ex: ParseException) {
+            Timber.e(ex)
+        }
+        return null
     }
 }
