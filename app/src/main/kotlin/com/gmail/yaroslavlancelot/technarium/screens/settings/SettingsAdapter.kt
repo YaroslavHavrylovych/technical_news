8d4072a69dc6e25e4697e6fd584cb5b8d956be21
Yaroslav Havrylovych <yaroslavlancelot@gmail.com>
2020-02-05 13:08:59 +0200

    Design: history and provider settings (design only)

---
+++ app/src/main/kotlin/com/gmail/yaroslavlancelot/technarium/screens/settings/SettingsAdapter.kt
@@ -0,0 +1,69 @@
+/**
+ * Copyright (C) 2020 Yaroslav Havrylovych
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.gmail.yaroslavlancelot.technarium.screens.settings
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.gmail.yaroslavlancelot.technarium.R
+import kotlinx.android.synthetic.main.lt_settings_item.view.selection_area_view
+import kotlinx.android.synthetic.main.lt_settings_item.view.subtitle
+import kotlinx.android.synthetic.main.lt_settings_item.view.title
+
+class SettingsAdapter(
+    private val settings: List<Setting>,
+    private var itemClickListener: (setting: Setting) -> Unit
+) : RecyclerView.Adapter<SettingsAdapter.SettingViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SettingViewHolder =
+        SettingViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.lt_settings_item, parent, false))
+
+    override fun getItemCount() = settings.count()
+
+    override fun onBindViewHolder(holder: SettingViewHolder, position: Int) {
+        val setting = settings[position]
+        holder.title.setText(titleProvider(setting))
+        holder.subTitle.setText(subtitleProvider(setting))
+        holder.clickArea.setOnClickListener { itemClickListener(setting) }
+    }
+
+    class SettingViewHolder(settingView: View) : RecyclerView.ViewHolder(settingView) {
+        val title: TextView = itemView.title
+        val subTitle: TextView = itemView.subtitle
+        val clickArea: View = itemView.selection_area_view
+    }
+
+    companion object {
+        fun titleProvider(setting: Setting) =
+            when (setting) {
+                Setting.HISTORY -> R.string.settings_history_title
+                Setting.DATA_PROVIDERS -> R.string.settings_provider_title
+            }
+
+        fun subtitleProvider(setting: Setting) =
+            when (setting) {
+                Setting.HISTORY -> R.string.settings_history_subtitle
+                Setting.DATA_PROVIDERS -> R.string.settings_provider_subtitle
+            }
+    }
+}
+
+enum class Setting {
+    HISTORY, DATA_PROVIDERS
+}
--- app/src/main/kotlin/com/gmail/yaroslavlancelot/technarium/screens/settings/SettingsFragment.kt
+++ app/src/main/kotlin/com/gmail/yaroslavlancelot/technarium/screens/settings/SettingsFragment.kt
@@ -18,15 +18,19 @@ package com.gmail.yaroslavlancelot.technarium.screens.settings
 
 import android.os.Bundle
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
+import android.widget.RadioGroup
+import androidx.annotation.IdRes
+import androidx.appcompat.app.AlertDialog
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.gmail.yaroslavlancelot.technarium.R
 import com.gmail.yaroslavlancelot.technarium.screens.base.BaseFragment
 import com.gmail.yaroslavlancelot.technarium.settings.AppSettings
 import com.gmail.yaroslavlancelot.technarium.settings.HistoryStorage
-import kotlinx.android.synthetic.main.lt_settings_fragment.history_spinner
-import java.lang.IllegalStateException
+import kotlinx.android.synthetic.main.lt_history_selection_fragment.view.radio_group
+import kotlinx.android.synthetic.main.lt_settings_fragment.settings_list
 import javax.inject.Inject
+
 
 class SettingsFragment : BaseFragment() {
     @Inject
@@ -36,24 +40,47 @@ class SettingsFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        history_spinner.adapter = ArrayAdapter<String>(requireContext(), android.R.layout.simple_spinner_item, resources.getStringArray(R.array.settings_history_values))
-        history_spinner.setSelection(appSettings.historyStorage.ordinal + 1)
-        history_spinner.onItemSelectedListener = HistorySpinnerListener()
+        settings_list.layoutManager = LinearLayoutManager(context)
+        settings_list.addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
+        settings_list.adapter = SettingsAdapter(listOf(Setting.HISTORY, Setting.DATA_PROVIDERS), ::onSettingSelected)
     }
 
-    private inner class HistorySpinnerListener : AdapterView.OnItemSelectedListener {
-        override fun onNothingSelected(parent: AdapterView<*>?) = throw IllegalStateException("Nothing is not a value of this spinner")
-
-        override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-            appSettings.historyStorage = when (id) {
-                1L -> HistoryStorage.FEW_MONTHS
-                2L -> HistoryStorage.HALF_A_YEAR
-                3L -> HistoryStorage.YEAR
-                else -> appSettings.historyStorage
+    private fun onSettingSelected(setting: Setting) {
+        when (setting) {
+            Setting.HISTORY -> {
+                val dialogBuilder: AlertDialog.Builder = AlertDialog.Builder(requireContext())
+                dialogBuilder.setTitle(R.string.settings_history_title)
+                val dialogView: View = layoutInflater.inflate(R.layout.lt_history_selection_fragment, null)
+                preselectHistory(dialogView.radio_group)
+                dialogView.radio_group.setOnCheckedChangeListener(::onHistoryChanged)
+                dialogBuilder.setView(dialogView)
+                dialogBuilder.create().show()
+            }
+            Setting.DATA_PROVIDERS -> {
+                //TODO not implemented yet
             }
         }
-
     }
+
+    private fun preselectHistory(radioGroup: RadioGroup) {
+        radioGroup.check(
+            when (appSettings.historyStorage) {
+                HistoryStorage.FEW_MONTHS -> R.id.a_few_months_button
+                HistoryStorage.HALF_A_YEAR -> R.id.half_a_year_button
+                HistoryStorage.YEAR -> R.id.year_button
+            }
+        )
+    }
+
+    private fun onHistoryChanged(@Suppress("UNUSED_PARAMETER") group: RadioGroup, @IdRes checkedId: Int) {
+        appSettings.historyStorage = when (checkedId) {
+            R.id.a_few_months_button -> HistoryStorage.FEW_MONTHS
+            R.id.half_a_year_button -> HistoryStorage.HALF_A_YEAR
+            R.id.year_button -> HistoryStorage.YEAR
+            else -> appSettings.historyStorage
+        }
+    }
+
     //TODO change theme
     //TODO change theme provider
     //TODO change items appear animation
